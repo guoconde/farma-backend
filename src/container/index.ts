118d34ac84e543fdb14402e4b872bdb480c50a65
guoconde <guoconde@gmail.com>
2023-01-07 18:26:58 -0300

    feat: implementing listUser on userController

--- src/container/index.ts
+++ src/container/index.ts
@@ -3,6 +3,7 @@ import { container } from "tsyringe";
 import { UserRepository } from "@/repositories";
 import { CreateUserUseCase } from "@/use-cases/user";
 import { UserAuthUseCase } from "@/use-cases/auth/auth-user";
+import { ListUsersUseCase } from "@/use-cases/user/list-users";
 
 container.registerSingleton<UserRepository>("UserRepository", UserRepository);
 
@@ -14,4 +15,9 @@ container.registerSingleton<CreateUserUseCase>(
 container.registerSingleton<UserAuthUseCase>(
   "UserAuthUseCase",
   UserAuthUseCase
+);
+
+container.registerSingleton<ListUsersUseCase>(
+  "ListUsersUseCase",
+  ListUsersUseCase
 );
---
+++ src/controllers/user/list-users.ts
@@ -0,0 +1,24 @@
+import { inject, injectable } from "tsyringe";
+import { Request, Response } from "express";
+
+import { ListUsersUseCase } from "@/use-cases/user/list-users";
+
+@injectable()
+export class ListUsersController {
+  constructor(
+    @inject("ListUsersUseCase")
+    private readonly listUsers: ListUsersUseCase
+  ) {}
+
+  async handle(request: Request, response: Response) {
+    const { name = "", page = 1, limit = 50 } = request.body;
+
+    const userPagination = await this.listUsers.execute({
+      limit: Number(limit),
+      page: Number(page),
+      name: String(name),
+    });
+
+    response.status(200).json(userPagination);
+  }
+}
